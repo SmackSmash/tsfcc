e8f845d505dda96993135571fd2ec3009069c79c
SmackSmash <danmoffat@gmail.com>
2019-11-15 15:32:21 +0000

    Restored manifest.json, added component to fetch properties from API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.scss';
 
 const App = props => {
-  return <div>App works!</div>;
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const properties = await axios.get('/api/properties');
+      setProperties(properties.data);
+    };
+    fetchProperties();
+  }, []);
+
+  if (!properties.length) {
+    return <div>No properties</div>;
+  }
+  return (
+    <div>
+      {properties.map(property => (
+        <h3 key={property._id}>{property.name}</h3>
+      ))}
+    </div>
+  );
 };
 
 export default App;
