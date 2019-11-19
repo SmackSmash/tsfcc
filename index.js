4c3994dd6c91d5672e1131d6dd8c482358163bec
SmackSmash <danmoffat@gmail.com>
2019-11-19 16:30:11 +0000

    Small formatting change to index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use('/api/properties', require('./routes/properties'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
