d764a9af75597b907869e0af5cff8ba2d2cdfb03
SmackSmash <danmoffat@gmail.com>
2019-11-19 17:19:33 +0000

    Added a small change to test updating files on the server

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = props => {
-  return <div>Home page works!</div>;
+  return (
+    <div>
+      Home page works! This is a minor change to see how quickly I can update the site... what about
+      static files?
+    </div>
+  );
 };
 
 export default Home;
