eac3beff91f4f8811033fef8d6fcc54d98fe9dd9
SmackSmash <danmoffat@gmail.com>
2019-11-22 16:32:24 +0000

    Added auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import propertiesReducer from './propertiesReducer';
+import authReducer from './authReducer';
 
 export default combineReducers({
-  properties: propertiesReducer
+  properties: propertiesReducer,
+  auth: authReducer
 });
