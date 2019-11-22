0d3683cfb544f1618ba6ca6073290ca393177a32
SmackSmash <danmoffat@gmail.com>
2019-11-22 15:01:12 +0000

    Added route to sign in admin users

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const { User, validateUser } = require('../models/users');
 const { JWT_SECRET } = require('../config/keys');
 
 // @route   POST /api/users/signup
-// @desc    Sign up admin users
+// @desc    Sign up admin user
 // @access  Public (temporary)
 router.post('/signup', async (req, res) => {
   const result = validateUser.validate(req.body, { abortEarly: false });
@@ -28,6 +28,41 @@ router.post('/signup', async (req, res) => {
       password: hashed
     });
     await user.save();
+    const payload = {
+      user: username
+    };
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '5h' });
+    res.send({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.sendStatus(500);
+  }
+});
+
+// @route   POST /api/users/signin
+// @desc    Sign in admin user
+// @access  Public
+router.post('/signin', async (req, res) => {
+  const result = validateUser.validate(req.body, { abortEarly: false });
+  if (result.error) {
+    return res.status(422).send({
+      errors: result.error.details.map(({ message }) => message)
+    });
+  }
+  const { username, password } = req.body;
+  try {
+    let user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).send({
+        errors: ['Invalid credentials']
+      });
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).send({
+        errors: ['Invalid  credentials']
+      });
+    }
     const payload = {
       user: username
     };
