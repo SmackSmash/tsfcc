b08764b35b02fd926ceb3e0d5ec528abed7e4a7b
SmackSmash <danmoffat@gmail.com>
2019-11-26 22:09:15 +0000

    Added flags to mongoose connect function to avoid deprecation warnings

--- services/dbConnect.js
+++ services/dbConnect.js
@@ -3,7 +3,12 @@ const { MONGO_URI } = require('../config/keys');
 
 module.exports = async () => {
   try {
-    await mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
     console.log('Connected to DB');
   } catch (error) {
     console.error(error.message);
